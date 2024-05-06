93f15eacfa334426edf1b70debdd19388f4e33a8
Ahmad Jundi <154776571+ahmadjundik@users.noreply.github.com>
2024-05-06 17:01:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,69 +22,70 @@ add_selectbox = st.sidebar.selectbox (
 )
 st.sidebar.info('Web ini digunakan untuk melihat prediksi churn bank berserta visualisasinya')
 st.sidebar.image(image2)
-def standardize_features(df):
-    """Standardize the numerical features."""
-    sc = StandardScaler()
-    return pd.DataFrame(sc.fit_transform(df), columns=df.columns)
+if add_selectbox == 'Machine Learning':
+  def standardize_features(df):
+      """Standardize the numerical features."""
+      sc = StandardScaler()
+      return pd.DataFrame(sc.fit_transform(df), columns=df.columns)
 
-def preprocess(df):
-    df = df.drop(columns = ['Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1',
+  def preprocess(df):
+      df = df.drop(columns = ['Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1',
                         'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2'])
-    df.drop(columns=['CLIENTNUM'], inplace=True)
+      df.drop(columns=['CLIENTNUM'], inplace=True)
 
-    # Configure to replace Unknow values with missing values
-    replace_un = {'Unknown': np.nan}
-    df['Education_Level'].replace(replace_un, inplace=True)
+      # Configure to replace Unknow values with missing values
+      replace_un = {'Unknown': np.nan}
+      df['Education_Level'].replace(replace_un, inplace=True)
 
-    # Manage missing value in Education_Level using SimpleImputer
-    imp1 = SimpleImputer(strategy="most_frequent")
-    df[['Education_Level']] = imp1.fit_transform(df[['Education_Level']])
+      # Manage missing value in Education_Level using SimpleImputer
+      imp1 = SimpleImputer(strategy="most_frequent")
+      df[['Education_Level']] = imp1.fit_transform(df[['Education_Level']])
 
-    # Configure to replace new variables
-    educt_lavel = {'Uneducated':0, 'High School':1, 'College':2,
+      # Configure to replace new variables
+      educt_lavel = {'Uneducated':0, 'High School':1, 'College':2,
                 'Graduate':3, 'Post-Graduate':4, 'Doctorate':5}
-    df.replace(educt_lavel, inplace=True)
+      df.replace(educt_lavel, inplace=True)
 
-    # Configure to replace Unknow values with missing values
-    replace_un = {'Unknown': np.nan}
-    df['Marital_Status'].replace(replace_un, inplace=True)
+      # Configure to replace Unknow values with missing values
+      replace_un = {'Unknown': np.nan}
+      df['Marital_Status'].replace(replace_un, inplace=True)
 
-    # Manage missing value in Marital status using SimpleImputer
-    imp2 = SimpleImputer(strategy="most_frequent")
-    df[['Marital_Status']] = imp2.fit_transform(df[['Marital_Status']])
+      # Manage missing value in Marital status using SimpleImputer
+      imp2 = SimpleImputer(strategy="most_frequent")
+      df[['Marital_Status']] = imp2.fit_transform(df[['Marital_Status']])
 
-    # Configure to replace new variables
-    marital_status = {'Single':0, 'Married':1, 'Divorced':2}
-    df.replace(marital_status, inplace=True)
+      # Configure to replace new variables
+      marital_status = {'Single':0, 'Married':1, 'Divorced':2}
+      df.replace(marital_status, inplace=True)
 
-    # Configure to replace Unknow values with missing values
-    replace_un = {'Unknown': np.nan}
-    df['Income_Category'].replace(replace_un, inplace=True)
+      # Configure to replace Unknow values with missing values
+      replace_un = {'Unknown': np.nan}
+      df['Income_Category'].replace(replace_un, inplace=True)
 
-    # Manage missing value in Income category using SimpleImputer
-    imp3 = SimpleImputer(strategy="most_frequent")
-    df[['Income_Category']] = imp3.fit_transform(df[['Income_Category']])
+      # Manage missing value in Income category using SimpleImputer
+      imp3 = SimpleImputer(strategy="most_frequent")
+      df[['Income_Category']] = imp3.fit_transform(df[['Income_Category']])
 
-    # Configure to replace new variables
-    income_cat = {'Less than $40K': 0, '$40K - $60K': 1, '$60K - $80K': 2,
-                '$80K - $120K': 3, '$120K +': 4}
-    df.replace(income_cat, inplace=True)
+      # Configure to replace new variables
+      income_cat = {'Less than $40K': 0, '$40K - $60K': 1, '$60K - $80K': 2,
+                  '$80K - $120K': 3, '$120K +': 4}
+      df.replace(income_cat, inplace=True)
 
-    # Configure to replace new variables
-    att_flag = {'Existing Customer':0, 'Attrited Customer':1}
-    df['Attrition_Flag'].replace(att_flag, inplace=True)
+      # Configure to replace new variables
+      att_flag = {'Existing Customer':0, 'Attrited Customer':1}
+      df['Attrition_Flag'].replace(att_flag, inplace=True)
 
-    # Configure to replace new variables
-    gender = {'F':0, 'M':1}
-    df['Gender'].replace(gender, inplace=True)
+      # Configure to replace new variables
+      gender = {'F':0, 'M':1}
+      df['Gender'].replace(gender, inplace=True)
 
-    # Configure to replace new variables
-    card_cat = {'Blue':0, 'Silver':1, 'Gold':2, 'Platinum':3}
-    df['Card_Category'].replace(card_cat, inplace=True)
+      # Configure to replace new variables
+      card_cat = {'Blue':0, 'Silver':1, 'Gold':2, 'Platinum':3}
+      df['Card_Category'].replace(card_cat, inplace=True)
 
-    # Scale the features
-    df = standardize_features(df)
-    return df
+      # Scale the features
+      df = standardize_features(df)
+      return df
 
 st.title("Bank Customer Churn Prediction")
 st.write("Upload a CSV file for prediction.")
