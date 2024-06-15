b00c3dd88031fe52465d85e0fcc57d74e8b04c27
Ahmad Jundi <154776571+ahmadjundik@users.noreply.github.com>
2024-06-15 11:17:13 +0700

    Update and rename score.py to deployment-script.py

--- deployment-script.py
+++ deployment-script.py
@@ -11,7 +11,7 @@ ws = Workspace.from_config()
 
 # Registrasikan model Anda
 model = Model.register(workspace=ws,
-                       model_path="Users/khairurrijal/get-started-notebooks/deploy/modelxgb.sav",
+                       model_path="Users/khairurrijal/project-directory/modelxgb.sav",  # Path relatif ke model Anda
                        model_name="modelxgboost")
 
 # Buat environment baru
@@ -26,14 +26,18 @@ env.python.conda_dependencies = deps
 # Registrasikan environment
 env.register(workspace=ws)
 
-# Verifikasi direktori sumber dan file score.py
-src_dir = os.path.join(os.getcwd(), 'src')
+# Memeriksa direktori kerja saat ini dan keberadaan direktori src serta file score.py
+current_working_directory = os.getcwd()
+print(f"Current working directory: {current_working_directory}")
+
+src_dir = os.path.join(current_working_directory, 'src')
 score_script = os.path.join(src_dir, 'score.py')
 
 if os.path.isdir(src_dir) and os.path.isfile(score_script):
+    print("Directory src and file score.py found.")
     inference_config = InferenceConfig(entry_script="score.py", source_directory="src", environment=env)
 else:
-    raise Exception("Directory src or file score.py not found.")
+    raise Exception("Directory src or file score.py not found. Please check the paths.")
 
 # Konfigurasi penyebaran
 deployment_config = AciWebservice.deploy_configuration(cpu_cores=1, memory_gb=1)
@@ -46,3 +50,4 @@ service = Model.deploy(workspace=ws,
                        deployment_config=deployment_config)
 
 service.wait_for_deployment(show_output=True)
+
